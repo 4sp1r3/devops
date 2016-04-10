6db9bbcc249f5f8166bc6bcb6f051ad07220aae1
4sp1r3 <4sp1r3@users.noreply.github.com>
2016-04-10 20:21:10 -0300

    Inicio

---
+++ prueba01.py
@@ -0,0 +1,17 @@
+import dns.resolver
+import requests
+
+
+#Get A records from imujer.com
+answers = dns.resolver.query('www.imujer.com', 'A')
+
+# For each 
+for rawdata in answers:
+    # get uri
+    uri = 'http://199.34.125.35/test?email=fcocaro%40gmail.com&record=' + str(rawdata)
+    
+    #perform request
+    r = requests.get(uri)
+    
+    #print results
+    print ('Request: %s -> Response: %s') % (uri, r.status_code)
