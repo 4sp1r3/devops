bf3b0951b48d7bc62c3c9ea41f376fd72251878d
4sp1r3 <4sp1r3@users.noreply.github.com>
2016-04-16 18:25:03 -0300

    Update and rename prueba01.py to imujer.py

--- imujer.py
+++ imujer.py
@@ -8,7 +8,7 @@ answers = dns.resolver.query('www.imujer.com', 'A')
 # For each 
 for rawdata in answers:
     # get uri
-    uri = 'http://199.34.125.35/test?email=fcocaro%40gmail.com&record=' + str(rawdata)
+    uri = 'http://199.34.125.35/test?email=me%40gmail.com&record=' + str(rawdata)
     
     #perform request
     r = requests.get(uri)
